528830b2e5695258f007bf26ffd36da998454fc1
RiverCastle <rkdtjd12@gmail.com>
2023-05-02 17:34:44 +0900

    feat: first commit -백준 별찍기8

---
+++ rivercastle/baekjoon/printStars/PrintStars8.java
@@ -0,0 +1,36 @@
+package rivercastle.baekjoon.printStars;
+
+import java.util.Scanner;
+
+public class PrintStars8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int iVal1 = scanner.nextInt();
+
+        for (int i = 0; i < iVal1; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("*");
+            }
+            for (int k = 0; k < 2 * (iVal1 - 1 - i); k++) {
+                System.out.printf(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i < iVal1; i++) {
+            for (int j = 0; j < iVal1 - i; j++) {
+                System.out.printf("*");
+            }
+            for (int k = 0; k < 2 * i; k++) {
+                System.out.printf(" ");
+            }
+            for (int j = 0; j < iVal1 - i; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
