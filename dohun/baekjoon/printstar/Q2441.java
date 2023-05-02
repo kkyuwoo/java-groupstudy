066536ec0178b5aff88664c21815e19f198d259d
dothur <craz1000@naver.com>
2023-05-02 17:15:46 +0900

    feat. 별 찍기 - 4

---
+++ dohun/baekjoon/printstar/Q2441.java
@@ -0,0 +1,20 @@
+package dohun.baekjoon.printstar;
+
+import java.util.Scanner;
+
+public class Q2441 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k < count; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
