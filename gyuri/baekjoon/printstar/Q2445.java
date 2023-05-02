a26f821431d1b66c4e0e8c4f4cf66a885573bd31
kkyuwoo <kyurikim012@gmail.com>
2023-05-02 17:27:41 +0900

    feat: 백준 별찍기8

---
+++ gyuri/baekjoon/printstar/Q2445.java
@@ -0,0 +1,34 @@
+package gyuri.baekjoon.printstar;
+
+import java.util.Scanner;
+
+public class Q2445 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            for(int k=1; k<=2*(n-i); k++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1; i>=1; i--) {
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            for(int k=1; k<=2*(n-i); k++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
