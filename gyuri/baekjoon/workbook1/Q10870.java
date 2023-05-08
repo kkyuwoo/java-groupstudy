d764652b60241cba2950fa18580a6e53d2654831
kkyuwoo <kyurikim012@gmail.com>
2023-05-08 16:59:52 +0900

    feat: 백준10870 피보나치 수 5

---
+++ gyuri/baekjoon/workbook1/Q10870.java
@@ -0,0 +1,17 @@
+package gyuri.baekjoon.workbook1;
+
+import java.util.Scanner;
+
+public class Q10870 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n+1];
+        for (int i=1; i<arr.length; i++) {
+            if (i==0) arr[0] = 0;
+            else if(i==1) arr[1] = 1;
+            else arr[i] = arr[i-1] + arr[i-2];
+        }
+        System.out.println(arr[n]);
+    }
+}
