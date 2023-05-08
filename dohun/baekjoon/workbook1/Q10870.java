c2be812e1809418f61fe812458d5e9621a0a6487
dothur <craz1000@naver.com>
2023-05-08 22:17:03 +0900

    feat. 피보나치 수 5 - B2 - 재귀 return fibonacci(n - 1) ...

---
+++ dohun/baekjoon/workbook1/Q10870.java
@@ -0,0 +1,21 @@
+package dohun.baekjoon.workbook1;
+
+import java.util.Scanner;
+
+public class Q10870 {
+    public static int fibonacci(int num) {
+        if (num == 0) {
+            return 0;
+        } else if (num == 1) {
+            return 1;
+        } else {
+            return fibonacci(num - 1) + fibonacci(num - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(fibonacci(num));
+    }
+}
