aa7f9b58b857be4e144e085347418f39ab6496ab
RiverCastle <rkdtjd12@gmail.com>
2023-05-24 18:01:25 +0900

    feat: 약수

--- rivercastle/baekjoon/workbook3/Q10828.java
+++ rivercastle/baekjoon/workbook3/Q10828.java
@@ -1,8 +1,6 @@
 package rivercastle.baekjoon.workbook3;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -10,8 +8,11 @@ public class Q10828 {
     public static void main(String[] args) throws IOException {
         Stack<Integer> intStack = new Stack<>();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
         int testNum = Integer.parseInt(bufferedReader.readLine());
         StringTokenizer stringTokenizer;
+
+
         for (int i = 0; i < testNum; i++) {
             String line = bufferedReader.readLine();
             if (line.contains(" ")) {
@@ -19,6 +20,7 @@ public class Q10828 {
                 if (stringTokenizer.nextToken().equals("push")) {
                     intStack.push(Integer.parseInt(stringTokenizer.nextToken()));
                 }
+
             } else if (line.equals("pop")) {
                 if (intStack.size() == 0) {
                     System.out.println(-1);
@@ -27,8 +29,10 @@ public class Q10828 {
                 }
             } else if (line.equals("size")) {
                 System.out.println(intStack.size());
+
             } else if (line.equals("empty")) {
                 System.out.println(intStack.isEmpty() ? 1 : 0);
+
             } else if (line.equals("top")) {
                 if (intStack.size() == 0) {
                     System.out.println(-1);
