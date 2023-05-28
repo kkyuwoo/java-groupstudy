bc750468b367e05bac77bb227d2e49f8d7a1f93b
RiverCastle <rkdtjd12@gmail.com>
2023-05-28 21:18:14 +0900

    feat: 프로그래머스 배열 만들기 5 -내용: 완료

---
+++ rivercastle/programmers/Solution181829.java
@@ -0,0 +1,15 @@
+package rivercastle.programmers;
+
+public class Solution181829 {
+    public int solution(int[][] board, int k) {
+        int answer = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (i + j <= k) {
+                    answer += board[i][j];
+                }
+            }
+        }
+        return answer;
+    }
+}
