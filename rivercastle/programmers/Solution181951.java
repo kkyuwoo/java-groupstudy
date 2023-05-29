2797389552f0342bc57228c66573bc6073ca2d35
RiverCastle <rkdtjd12@gmail.com>
2023-05-29 18:25:35 +0900

    feat: a와 b 출력하기 -내용: 완료

---
+++ rivercastle/programmers/Solution181951.java
@@ -0,0 +1,14 @@
+package rivercastle.programmers;
+
+import java.util.Scanner;
+
+public class Solution181951 {
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.printf("a = %d\n", sc.nextInt());
+            System.out.printf("b = %d\n", sc.nextInt());
+        }
+    }
+}
