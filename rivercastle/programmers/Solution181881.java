164d10cf82a90592889ad1a07fd73d56ed45500b
RiverCastle <rkdtjd12@gmail.com>
2023-05-29 12:28:48 +0900

    feat: 조건에 맞게 수열 변환하기 2 -내용: 완료

--- rivercastle/programmers/Solution181881.java
+++ rivercastle/programmers/Solution181881.java
@@ -1,32 +1,57 @@
 package rivercastle.programmers;
 
+
 import java.util.Arrays;
 
 public class Solution181881 {
     public int[] changeArr(int[] arr) {
+        int[] temp = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= 50 && arr[i] % 2 == 0) {
-                arr[i] /= 2;
+                temp[i] = arr[i] / 2;
             } else if (arr[i] < 50 && arr[i] % 2 == 1) {
-                arr[i] = arr[i] * 2 + 1;
+                temp[i] = arr[i] * 2 + 1;
+            } else {
+                temp[i] = arr[i];
             }
         }
-        return arr;
+        return temp;
+    }
+
+    public boolean judge(int[] arr, int[] temp) { //같으면 true / 다르면 false
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != temp[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public int solution(int[] arr) {
         int answer = 0;
-        while (arr != changeArr(arr)) {
-            arr = changeArr(arr);
+        int[] temp = changeArr(arr);
+
+
+        while (!judge(arr, temp)) {
+            /*
+            System.out.printf("arr = ");
+            System.out.println(Arrays.toString(arr));
+            System.out.printf("temp = ");
+            System.out.println(Arrays.toString(temp));
+            System.out.println("answer = " + answer);
+            */
+
+            arr = temp;
+            temp = changeArr(arr);
             answer += 1;
         }
         return answer;
     }
-
+    /*
     public static void main(String[] args) {
         int[] arr = new int[] {1, 2, 3, 100, 99, 98};
         Solution181881 solution181881 = new Solution181881();
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(solution181881.changeArr(arr)));
+        System.out.println(solution181881.solution(arr));
     }
+     */
 }
