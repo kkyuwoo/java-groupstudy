bba18316ab0cadb11b4221706637ec3010cde4cb
RiverCastle <rkdtjd12@gmail.com>
2023-06-01 23:33:00 +0900

    feat: 단어 정렬 -내용: 정렬하기 comparator

---
+++ rivercastle/baekjoon/kangseong/Q1181.java
@@ -0,0 +1,26 @@
+package rivercastle.baekjoon.kangseong;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Q1181 { //단어 정렬 미해결
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int wordsNum = Integer.parseInt(bufferedReader.readLine());
+        PriorityQueue<String> wordsPriorityQueue = new PriorityQueue<>();
+        for (int i = 0; i < wordsNum; i++) {
+            wordsPriorityQueue.add(bufferedReader.readLine());
+        }
+        bufferedReader.close();
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < wordsNum; i++) {
+            bufferedWriter.write(wordsPriorityQueue.poll() + "\n");
+        }
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
