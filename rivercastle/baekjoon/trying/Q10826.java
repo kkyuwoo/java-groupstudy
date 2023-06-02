bb0f33522c2ba0f60dd4daeb4a02db44e91520c0
RiverCastle <rkdtjd12@gmail.com>
2023-06-02 23:45:53 +0900

    feat: 피보나치수열 4 성공

---
+++ rivercastle/baekjoon/trying/Q10826.java
@@ -0,0 +1,29 @@
+package rivercastle.baekjoon.trying;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q10826 {//피보나치수열 4
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int target = scanner.nextInt();
+
+        if (target > 1) {
+            BigInteger answer = new BigInteger("0");
+            BigInteger num1 = new BigInteger("0");
+            BigInteger num2 = new BigInteger("1");
+            for (int i = 2; i <= target; i++) {
+                answer = num1.add(num2);
+                num1 = num2;
+                num2 = answer;
+            }
+            System.out.println(answer);
+        } else {
+            int[] answer = new int[2];
+            answer[0] = 0;
+            answer[1] = 1;
+            System.out.println(answer[answer[target]]);
+        }
+    }
+}
