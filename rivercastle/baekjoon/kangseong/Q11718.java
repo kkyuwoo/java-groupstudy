f9361cb69386b0a8341f16fde8bd9bb3b8984fe6
RiverCastle <rkdtjd12@gmail.com>
2023-06-02 22:46:47 +0900

    feat: 그대로 출력하기 해결

---
+++ rivercastle/baekjoon/kangseong/Q11718.java
@@ -0,0 +1,13 @@
+package rivercastle.baekjoon.kangseong;
+
+
+import java.io.*;
+public class Q11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
